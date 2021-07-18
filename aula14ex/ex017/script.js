704c3b8473d5d98c031d51eafed70da5e1c04868
Washington95 <wst-washington@hotmail.com>
2021-07-18 20:55:37 -0300

    tabauda js

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,1 +1,19 @@
-alert('Olá !')
+function tabuada() {
+    let num = document.getElementById('txnum')
+    let tabu = document.getElementById('tab')
+    if (num.value.length == 0 ){
+        window.alert('Por favor, digite um número !')
+    }else {
+        let n = Number(num.value)
+        let c = 0
+        tabu.innerHTML= ''
+        while (c <= 10){
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tabu${c}`
+            tabu.appendChild(item)
+            c++
+        }
+
+    }
+}
